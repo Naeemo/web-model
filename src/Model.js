0fcccc2a06a2db8bd81f32fac676d153a253034d
naeemo <370552604@qq.com>
2017-03-10 20:56:38 +0800

    refactor use factory.

--- src/Model.js
+++ src/Model.js
@@ -20,15 +20,74 @@ superAgent.Request.prototype.escape = function () {
 
 
 /**
+ * run beforeInterceptors
+ * 前置拦截器
+ * wait for next() call: next(true) continue, next(false) break;
+ */
+function beforeHook(_request, beforeArr) {
+    return new Promise(function (resolve, reject) {
+        
+        if (_request._escape) resolve();
+        
+        let len = beforeArr.length;
+        
+        function next(ok = true, msg) {
+            
+            !ok && reject(msg);
+            
+            if (len--) {
+                if (!beforeArr[len]) return next();
+                beforeArr[len].call(_request, next);
+            } else {
+                resolve()
+            }
+        }
+        
+        next()
+        
+    })
+}
+
+
+/**
+ * run afterInterceptors
+ * 后置过滤器
+ * 拦截器显式返回一个false, 则中止循环。
+ */
+function afterHook(resolve, reject, afterArr) {
+    return function (err, res) {
+        
+        let _request = this;
+        
+        let len = afterArr.length;
+        let afterEscape = _request._escape;
+        while (!afterEscape && len--) {
+            
+            // skip invalid ones
+            if (!afterArr[len]) continue;
+            
+            let result = afterArr[len].call(_request, err, res);
+            
+            // 拦截器显式返回一个false, 则中止请求。
+            if (typeof result !== 'undefined' && !result) {
+                reject('after hook')
+            }
+            
+        }
+        
+        resolve(res);
+        
+    }
+}
+
+
+/**
  * model:
  * _base
  * request as tool
  *
- *
  * _beforeArr
  * _afterArr
- * _beforeHook
- * _afterHook
  *
  * apis
  *
@@ -40,10 +99,10 @@ export default class Model {
     static afterEach;
     
     constructor({
-        base = '',          // string
+        base = '',   // string
         beforeEach,  // function, before request
         afterEach,   // function, filter response
-        api = {}            // object, all requests of a model
+        api = {}     // object, all requests of a model
     }) {
         
         let model = this;
@@ -70,67 +129,6 @@ export default class Model {
         model._afterArr = [Model.afterEach, afterEach];
         
         
-        /**
-         * run beforeInterceptors
-         * 前置拦截器
-         * wait for next() call: next(true) continue, next(false) break;
-         */
-        model._beforeHook = function (_request) {
-            return new Promise(function (resolve, reject) {
-                
-                if (_request._escape) resolve();
-                
-                let len = model._beforeArr.length;
-                
-                function next(ok = true, msg) {
-                    
-                    !ok && reject(msg);
-                    
-                    if (len--) {
-                        if (!model._beforeArr[len]) return next();
-                        model._beforeArr[len].call(_request, next);
-                    } else {
-                        resolve()
-                    }
-                }
-                
-                next()
-                
-            })
-        };
-        
-        
-        /**
-         * run afterInterceptors
-         * 后置过滤器
-         * 拦截器显式返回一个false, 则中止循环。
-         */
-        model._afterHook = function (resolve, reject) {
-            return function (err, res) {
-                
-                let _request = this;
-                
-                let len = model._afterArr.length;
-                let afterEscape = _request._escape;
-                while (!afterEscape && len--) {
-                    
-                    // skip invalid ones
-                    if (!model._afterArr[len]) continue;
-                    
-                    let result = model._afterArr[len].call(_request, err, res);
-                    
-                    // 拦截器显式返回一个false, 则中止请求。
-                    if (typeof result !== 'undefined' && !result) {
-                        reject('after hook')
-                    }
-                    
-                }
-                
-                resolve(res);
-                
-            }
-        };
-        
         // attach the APIs
         for (let key in api) {
             // api[key];
@@ -139,7 +137,7 @@ export default class Model {
                     
                     let _request = api[key].apply(model, args);
                     
-                    model._beforeHook(_request).then(() => {
+                    beforeHook(_request, model._beforeArr).then(() => {
                         
                         // wire the base url if necessary:
                         // any string do not begin with 'http(s)://' or '//' will be wired
@@ -148,7 +146,7 @@ export default class Model {
                         }
                         
                         // make request
-                        _request.end(model._afterHook(resolve, reject).bind(_request))
+                        _request.end(afterHook(resolve, reject, model._afterArr).bind(_request))
                         
                     }).catch(e => {
                     });
