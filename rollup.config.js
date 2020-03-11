bdcfd57a6f87ee99d195a7a31045b603c265177f
naeemo <370552604@qq.com>
2020-03-11 11:17:40 +0800

    config update dev-dependencies, bump minor version.
    Signed-off-by: naeemo <370552604@qq.com>

--- rollup.config.js
+++ rollup.config.js
@@ -3,16 +3,16 @@
  */
 import babel from "rollup-plugin-babel";
 import {eslint} from "rollup-plugin-eslint";
-import resolve from "rollup-plugin-node-resolve";
-import commonjs from "rollup-plugin-commonjs";
-import replace from "rollup-plugin-replace";
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import replace from "@rollup/plugin-replace";
 import {terser} from "rollup-plugin-terser";
 
 export default {
     input: "src/Model.js",
     output: {
         file: "dist/model.js",
-        sourceMap: "inline",
+        sourcemap: "hidden",
         format: "es"
     },
     plugins: [
