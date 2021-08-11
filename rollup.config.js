73abf572b10f0180e7d6ab1ffbfa8e236de51e0d
Naeemo <370552604@qq.com>
2021-08-11 21:43:21 +0800

    config update dev deps.
    Signed-off-by: Naeemo <370552604@qq.com>

--- rollup.config.js
+++ rollup.config.js
@@ -5,7 +5,6 @@ import babel from "@rollup/plugin-babel";
 import {eslint} from "rollup-plugin-eslint";
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
-import replace from "@rollup/plugin-replace";
 import {terser} from "rollup-plugin-terser";
 
 export default {
@@ -27,9 +26,6 @@ export default {
             exclude: "node_modules/**",
         }),
         commonjs(),
-        replace({
-            ENV: JSON.stringify(process.env.NODE_ENV || "development"),
-        }),
         (process.env.NODE_ENV === "production" && terser()),
     ],
 };
